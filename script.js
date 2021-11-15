d8ba0c18ebf3020a0ca7c4c684525fac81e285d0
Paras Sharma <75166096+imparassharma@users.noreply.github.com>
2021-11-15 20:04:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,34 @@ const btns = document.querySelector(".col");
 const share = Array.from(btns.children);
 console.log(share)
 
+const error = document.querySelector(".error");
 
 
-function getshare(){
-    share.forEach((btn,index)=>{
-        btn.addEventListener("click",function(){
-            share.forEach((btn)=>{
-                btn.classList.remove("active");
-            })
-            if(btn.id!="input-share"){
-                var btn_value = btn.innerHTML.replace("%","");
-                btn.classList.toggle("active");
-                console.log(btn_value)
-                return btn_value;
-            }
-            else{
-                btn_value = getinputshare();
-                btn.addEventListener("click",function(){
-                    btn.classList.toggle("active-input");
-                    console.log(btn_value)
-                    return btn_value;
-                })
-            }
-    
+let share_value;
+share.forEach(btn=>{
+    btn.addEventListener("click",function(){
+        share.forEach((btn)=>{
+            btn.classList.remove("active");
         })
+        if(btn.id!="input-share"){
+            let btn_value = btn.innerHTML.replace("%","");
+            btn.classList.toggle("active");
+                share_value = btn_value;
+         }
+        else{
+            if(btn.style.backgroundColor != "hsl(172, 74%, 63%)"){
+                error.classList.add("show");
+            }
+            btn_value = getinputshare();
+            btn.addEventListener("click",function(){
+                btn.classList.add("active-input");
+                share_value = btn_value;
+                error.classList.remove("show");
+            })
+        }  
     })
-}
+        
+})
 
 
 function getinputshare(){
@@ -37,31 +39,46 @@ function getinputshare(){
 }
 
 function getbill(){
-    var bill_value = document.getElementById("bill").value;
-    console.log(bill_value)
+    let bill_value = document.getElementById("bill").value;
     return bill_value;
 }
 
 
 function getpeople(){
-    var people_value = document.getElementById("people").value;
-    console.log(people_value)
+    let people_value = document.getElementById("people").value;
     return people_value;
 }
 
 
+const tip_perperson = document.getElementById("tip-amount");
+const bill_perperson = document.getElementById("total-bill");
 
 function tipcalculate(){
-    var a = getbill();
-    var b = getshare();
-    var c = getpeople();
-    var tip_amount = (a*b)/(100*c);
+    let a = getbill();
+    console.log(a);
+    let b = share_value;
+    console.log(b);
+    let c = getpeople();
+    console.log(c);
+    let tip_amount = (a*b)/(100*c);
     console.log(tip_amount)
+
+    let total_bill = (tip_amount+parseInt(a)/c);
+
+    tip_perperson.innerHTML = "$"+tip_amount.toFixed(2);
+    bill_perperson.innerHTML = "$"+total_bill.toFixed(2);
 }
 
 const reset = document.getElementById("reset");
 
 reset.addEventListener("click",function(){
-    tipcalculate();
+    if(reset.innerHTML!= "Calculate"){
+        location.reload();
+    }
+    else{
+        tipcalculate();
+        reset.innerHTML = "Reset";
+    }
 })
+
 
